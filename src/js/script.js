39b7b84346ac2bf3a90928f1eeef78d6c8c0036c
Lyutik13 <lyutikov.viktor.96@mail.ru>
2023-03-08 22:30:07 +0300

    ferst

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    rewind: true,
+
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
